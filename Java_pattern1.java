d80f00205b2395e10d2f84abd29d69a7c3beb3e8
Gulzar Singh <gsingh29051982@gmail.com>
2023-08-24 12:01:36 -0400

    Updating 2nd code

---
+++ Java_pattern1.java
@@ -0,0 +1,32 @@
+package test;
+/*Code for pattern
+
+ ******
+******
+******
+******
+******
+******
+*/
+public class Java_pattern1 {
+
+	public static void main(String[] args) {
+		int n=5;
+		int c=n;
+		for (int i=0; i<=n;i++){
+			for (int j=0; j<=n-i;j++){
+				
+				System.out.print(" ");}
+			
+			for(int j=0;j<=n;j++)
+				
+				{System.out.print("*");}
+			
+			
+			System.out.println();
+		}
+		
+				
+		}
+
+}
